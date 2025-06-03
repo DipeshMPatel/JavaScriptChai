7e61f05f28a0b6e836b720ff42998b36a3b22e64
Dipesh Patel <141577046+DipeshMPatel@users.noreply.github.com>
2025-06-03 13:36:56 +0000

    Comparison

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);          //Bad Practice
+// console.log(null == 0);         //Bad Practice
+// console.log(null >= 0);         //Bad Practice
+
+// console.log(undefined == 0);   //Bad Practice
+// console.log(undefined > 0);    //Bad Practice
+// console.log(undefined < 0);    //Bad Practice
+
+// === (Does not checks just value, it also checks the data type)
+
+console.log("2" == 2);       //true
+console.log("2" === 2);      //false
